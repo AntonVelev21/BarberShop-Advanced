15892d5ca0905b74511f5a6519b5293a2afa5b13
AntonVelev21 <velevanton80@gmail.com>
2026-04-15 15:48:38 +0300

    Altered BookingForm tests adding dinamic data logic for date_and_hour field following the DRY priniples.

--- bookings/tests/forms.py
+++ bookings/tests/forms.py
@@ -1,6 +1,9 @@
+from datetime import datetime, timedelta
 import os
+from time import strftime
 
 import django
+from celery.utils.time import weekday
 from django.test import TestCase
 
 from bookings.forms import BookingCreateForm, BookingEditForm
@@ -45,9 +48,21 @@ class BookingFormTests(TestCase):
             image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxOFvr7KdOLpK-eBEQC4MZ5w9bP88Nppxklw&s'
         )
 
+        now = datetime.now()
+        if now.weekday() == 6:
+            now = timedelta(days=1) + now
+        self.valid_time_and_date = now.replace(year=now.year, month=now.month, day=now.day, hour=14, minute=30)
+        self.formated_now = now.strftime("%Y-%m-%d %H:%M")
+
+        self.formated_now_with_invalid_hour = self.valid_time_and_date.replace(minute=35).strftime("%Y-%m-%d %H:%M")
+        self.formated_now_with_hour_outside_of_work_hours = self.valid_time_and_date.replace(hour=21).strftime("%Y-%m-%d %H:%M")
+        days_until_sunday = 7 - self.valid_time_and_date.isoweekday()
+        self.formated_now_with_day_outside_of_work_days = (self.valid_time_and_date + timedelta(days_until_sunday)).strftime("%Y-%m-%d %H:%M")
+
+
     def test_booking_create_with_valid_data(self):
         form_data = {
-            'date_and_hour': '2026-04-15 14:30',
+            'date_and_hour': self.valid_time_and_date,
             'barber': self.barber.id,
             'services': [self.service1.id, self.service2.id]
         }
@@ -58,64 +73,119 @@ class BookingFormTests(TestCase):
 
     def test_booking_create_with_invalid_hour(self):
         form_data_a = {
-            'date_and_hour': '2026-04-15 14:35', #The hour is not in allowed hours (only between 00 and 30)
+            'date_and_hour': self.formated_now_with_invalid_hour, #The hour is not in allowed hours (only between 00 and 30)
             'barber': self.barber.id,
             'services': [self.service1.id, self.service2.id]
         }
 
         form_a = BookingCreateForm(data=form_data_a)
         self.assertFalse(form_a.is_valid())
+        self.assertIn('date_and_hour', form_a.errors)
+        self.assertEqual(
+            form_a.errors['date_and_hour'][0],
+            'Please select a valid time slot (e.g. 10:00 or 10:30).'
+        )
 
         form_data_b = {
-            'date_and_hour': '2026-04-15 20:30', #The hour is not in working hours (09:00 - 20:00)
+            'date_and_hour': self.formated_now_with_hour_outside_of_work_hours, #The hour is not in working hours (09:00 - 20:00)
             'barber': self.barber.id,
             'services': [self.service1.id, self.service2.id]
         }
 
         form_b = BookingCreateForm(data=form_data_b)
         self.assertFalse(form_b.is_valid())
+        self.assertIn('date_and_hour', form_b.errors)
+        self.assertEqual(
+            form_b.errors['date_and_hour'][0],
+            'Our working hours are between 09:00 and 18:00.'
+        )
 
 
     def test_booking_create_with_rest_day(self):
         form_data = {
-            'date_and_hour': '2026-04-19 14:30', #The day is not in working days (Monday - Saturday: 09:00 - 20:00)
+            'date_and_hour': self.formated_now_with_day_outside_of_work_days, #The day is not in working days (Monday - Saturday: 09:00 - 20:00)
             'barber': self.barber.id,
             'services': [self.service1.id, self.service2.id]
         }
 
         form = BookingCreateForm(data=form_data)
         self.assertFalse(form.is_valid())
+        self.assertIn('date_and_hour', form.errors)
+        self.assertEqual(
+            form.errors['date_and_hour'][0],
+            'Please choose a working day. We work from Monday to Saturday.'
+        )
 
+    def test_booking_create_in_the_past(self):
+        form_data = {
+            'date_and_hour': '2026-04-04 14:30',
+            'barber': self.barber.id,
+            'services': [self.service1.id, self.service2.id]
+        }
+        form = BookingCreateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('date_and_hour', form.errors)
+        self.assertEqual(
+            form.errors['date_and_hour'][0],
+            'You cannot book a seat in the past!'
+        )
 
     def test_booking_create_when_booking_on_the_date_and_hour_with_the_same_barber_exists(self):
         booking = Booking.objects.create(
             user_profile=self.user.user_profile,
             barber=self.barber,
-            date_and_hour='2026-04-14 14:30'
+            date_and_hour=self.valid_time_and_date
         )
 
         form_data = {
-            'date_and_hour': '2026-04-14 14:30', #The date_and_hour field is already used in other instance with the same barber
+            'date_and_hour': self.valid_time_and_date, #The date_and_hour field is already used in other instance with the same barber
             'barber': self.barber.id,
             'services': [self.service1.id, self.service2.id]
         }
 
         form = BookingCreateForm(data=form_data)
         self.assertFalse(form.is_valid())
+        self.assertIn('date_and_hour', form.errors)
+        self.assertEqual(
+            form.errors['date_and_hour'][0],
+            'Sorry, test3 is already booked for this time. Please choose another.'
+        )
 
 
     def test_booking_edit_with_the_same_time_is_valid(self):
         booking = Booking.objects.create(
             user_profile=self.user.user_profile,
             barber=self.barber,
-            date_and_hour='2026-04-15 14:30'
+            date_and_hour=self.valid_time_and_date
         )
 
         form_data = {
-            'date_and_hour': '2026-04-15 14:30',
+            'date_and_hour': self.valid_time_and_date,
             'barber': self.barber.id,
             'services': [self.service1.id, self.service2.id]
         }
 
         form = BookingEditForm(data=form_data, instance=booking)
         self.assertTrue(form.is_valid())
+
+
+    def test_booking_edit_with_invalid_data(self):
+        booking = Booking.objects.create(
+            user_profile=self.user.user_profile,
+            barber=self.barber,
+            date_and_hour=self.valid_time_and_date
+        )
+
+        form_data = {
+            'date_and_hour': self.formated_now_with_invalid_hour, #The hour is invalid
+            'barber': self.barber.id,
+            'services': [self.service1.id, self.service2.id]
+        }
+
+        form = BookingEditForm(data=form_data, instance=booking)
+        self.assertFalse(form.is_valid())
+        self.assertIn('date_and_hour', form.errors)
+        self.assertEqual(
+            form.errors['date_and_hour'][0],
+            'Please select a valid time slot (e.g. 10:00 or 10:30).'
+        )
